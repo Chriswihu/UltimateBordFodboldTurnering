8086ea5b28ef2cb3b50c891d6a936e5b5c7ec889
Aleks1075 <buha_5@hotmail.com>
2022-04-06 07:21:54 +0200

    Added TODO tasks

--- src/UltimateBordFodboldTurnering/Main.java
+++ src/UltimateBordFodboldTurnering/Main.java
@@ -8,3 +8,8 @@ public class Main {
         Tournament.runTournament();
     }
 }
+
+//TODO:
+// 1. Fremfor arraylists skal de gemmes per linje, så de kan læses med filewriteren
+// 2. Når man opretter turnering skal man kunne planlægge alle 7 kampe.
+// 3. Få gemt kampresultater i en tekst fil, som skal kunne læses via menuen.
